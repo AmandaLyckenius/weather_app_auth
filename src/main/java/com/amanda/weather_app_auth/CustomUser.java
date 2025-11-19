06c41bd01e8c1d347e0abf7488934ab2ebadec08
amandalyckenius <alyckenius@gmail.com>
2025-11-19 11:56:06 +0100

    feat(CustomUser): add CustomUserDetails and matching attributes in CustomUser

--- src/main/java/com/amanda/weather_app_auth/CustomUser.java
+++ src/main/java/com/amanda/weather_app_auth/CustomUser.java
@@ -14,12 +14,53 @@ public class CustomUser {
     private String username;
     private String password;
     private String email;
+    private boolean isAccountNonExpired;
+    private boolean isAccountNonLocked;
+    private boolean isCredentialsNonExpired;
+    private boolean isEnabled;
 
     public CustomUser(){}
-    public CustomUser(String username, String password, String email) {
+
+    public CustomUser(String username, String password, String email, boolean isAccountNonExpired, boolean isAccountNonLocked, boolean isCredentialsNonExpired, boolean isEnabled) {
         this.username = username;
         this.password = password;
         this.email = email;
+        this.isAccountNonExpired = isAccountNonExpired;
+        this.isAccountNonLocked = isAccountNonLocked;
+        this.isCredentialsNonExpired = isCredentialsNonExpired;
+        this.isEnabled = isEnabled;
+    }
+
+    public boolean isAccountNonExpired() {
+        return isAccountNonExpired;
+    }
+
+    public void setAccountNonExpired(boolean accountNonExpired) {
+        isAccountNonExpired = accountNonExpired;
+    }
+
+    public boolean isAccountNonLocked() {
+        return isAccountNonLocked;
+    }
+
+    public void setAccountNonLocked(boolean accountNonLocked) {
+        isAccountNonLocked = accountNonLocked;
+    }
+
+    public boolean isCredentialsNonExpired() {
+        return isCredentialsNonExpired;
+    }
+
+    public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+        isCredentialsNonExpired = credentialsNonExpired;
+    }
+
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        isEnabled = enabled;
     }
 
     public UUID getId() {
---
+++ src/main/java/com/amanda/weather_app_auth/CustomUserDetails.java
@@ -0,0 +1,51 @@
+package com.amanda.weather_app_auth;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final CustomUser customUser;
+
+    public CustomUserDetails(CustomUser customUser) {
+        this.customUser = customUser;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return customUser.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return customUser.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return customUser.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return customUser.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return customUser.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return customUser.isEnabled();
+    }
+}
