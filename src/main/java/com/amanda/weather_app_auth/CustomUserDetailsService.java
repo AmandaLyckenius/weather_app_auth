3287c622c23d1ea079391a5ece2d9f7474e115ea
amandalyckenius <alyckenius@gmail.com>
2025-11-19 14:10:06 +0100

    feat(auth): add CustomUserDetailsService and CustomUserRepository

---
+++ src/main/java/com/amanda/weather_app_auth/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.amanda.weather_app_auth;
+
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final CustomUserRepository customUserRepository;
+
+    public CustomUserDetailsService(CustomUserRepository customUserRepository) {
+        this.customUserRepository = customUserRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+
+        CustomUser customUser = customUserRepository.findUserByUsername(username)
+                .orElseThrow(
+                        () -> new UsernameNotFoundException("User with username " + username + " could not be found")
+                );
+
+
+        return new CustomUserDetails((customUser));
+    }
+}
---
+++ src/main/java/com/amanda/weather_app_auth/CustomUserRepository.java
@@ -0,0 +1,10 @@
+package com.amanda.weather_app_auth;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CustomUserRepository extends JpaRepository<CustomUser, UUID> {
+    Optional<CustomUser> findUserByUsername(String username);
+}
