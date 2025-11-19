4b3ca3b83ade05bd1aefbd0469f7f44730e50034
amandalyckenius <alyckenius@gmail.com>
2025-11-19 11:24:30 +0100

    initial commit

---
+++ src/main/java/com/amanda/weather_app_auth/WeatherAppAuthApplication.java
@@ -0,0 +1,13 @@
+package com.amanda.weather_app_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAppAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAppAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amanda/weather_app_auth/WeatherAppAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amanda.weather_app_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAppAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
