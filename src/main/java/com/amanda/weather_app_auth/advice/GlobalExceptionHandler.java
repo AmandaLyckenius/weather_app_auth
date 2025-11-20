63f1f4020142320cacf1abcd99f84ec43c3ede74
amandalyckenius <alyckenius@gmail.com>
2025-11-20 16:47:51 +0100

    feat(excaption): add globalExceptionHandler with userNotFound and general exception

---
+++ src/main/java/com/amanda/weather_app_auth/advice/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.amanda.weather_app_auth.advice;
+
+import com.amanda.weather_app_auth.exception.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFound(UserNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong: " + e.getMessage());
+    }
+}
