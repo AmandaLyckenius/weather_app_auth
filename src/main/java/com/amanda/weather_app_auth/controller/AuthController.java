2150aa99d766289eaedd9717295048559646f997
amandalyckenius <alyckenius@gmail.com>
2025-11-25 15:05:02 +0100

    feat(auth): add /logout

--- src/main/java/com/amanda/weather_app_auth/controller/AuthController.java
+++ src/main/java/com/amanda/weather_app_auth/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.amanda.weather_app_auth.dto.CustomUserLoginDTO;
 import com.amanda.weather_app_auth.dto.CustomUserLoginResponseDTO;
 import com.amanda.weather_app_auth.dto.CustomUserResponseDTO;
 import com.amanda.weather_app_auth.service.AuthService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -49,6 +50,21 @@ public class AuthController {
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
                 .body(responseDTO);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout() {
+        ResponseCookie cookie = ResponseCookie.from("jwt", "")
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .maxAge(0)
+                .sameSite("Lax")
+                .build();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body("Logged out successfully");
+
     }
 
 
