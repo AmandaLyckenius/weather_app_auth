4cfe3cbdbf361ce99759de4cceb62110b45814bb
amandalyckenius <alyckenius@gmail.com>
2025-11-26 14:42:27 +0100

    add: logging in handleUserLookup

--- src/main/java/com/amanda/weather_app_auth/service/UserLookupListener.java
+++ src/main/java/com/amanda/weather_app_auth/service/UserLookupListener.java
@@ -6,6 +6,8 @@ import com.amanda.weather_app_auth.dto.UserLookupResponseDTO;
 import com.amanda.weather_app_auth.exception.UserNotFoundException;
 import com.amanda.weather_app_auth.user.CustomUser;
 import com.amanda.weather_app_auth.user.CustomUserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,7 @@ import org.springframework.stereotype.Service;
 public class UserLookupListener {
     private final CustomUserRepository customUserRepository;
     private final RabbitTemplate rabbitTemplate;
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
 
     public UserLookupListener(CustomUserRepository customUserRepository, RabbitTemplate rabbitTemplate) {
@@ -23,23 +26,20 @@ public class UserLookupListener {
 
     @RabbitListener(queues = RabbitConfig.REQUEST_QUEUE)
     public void handleUserLookup(UserLookupRequestDTO userLookupRequestDTO){
-
+        log.info("--Listener starting--");
         System.out.println("🔥 Received UserLookupRequestDTO: " + userLookupRequestDTO);
         System.out.println("🔥 userId = " + userLookupRequestDTO.userId());
 
         if (userLookupRequestDTO.userId() == null) {
             System.err.println("❌ userId i UserLookupRequestDTO är null – kan inte kalla findById");
-            // Här kan du antingen return:a, eller kasta en mer beskrivande exception
             return;
-            // eller:
-            // throw new IllegalArgumentException("userId must not be null");
         }
 
         CustomUser user = customUserRepository.findByUserId(userLookupRequestDTO.userId())
                 .orElseThrow(() -> new UserNotFoundException(userLookupRequestDTO.userId()));
 
         UserLookupResponseDTO responseDTO = new UserLookupResponseDTO(user.getEmail());
-
+        log.info("--Sending email--");
         rabbitTemplate.convertAndSend(
                 RabbitConfig.EXCHANGE,
                 RabbitConfig.RESPONSE_ROUTING_KEY,
